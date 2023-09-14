c3e2fbc37cd279cc0ae32341fe7a6e24f9df8264
Музыкова Валентина Леонидовна <10221285@college.local>
2023-09-14 20:31:47 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println("ВВедетие последовательность")
+    val r = readln()
+    var currentVal = r[0]
+    var count = 1
+    var itog = ""
+    for (item in r.substring(1)) {
+        if (currentVal == item) {
+            count++
+        } else {
+            if (count == 1) {
+                itog += currentVal
+            } else {
+                itog += "$currentVal$count"
+            }
+            count = 1
+            currentVal = item
+        }
+    }
+    println(itog)
+}
