a7e0277c83184556c6d46b16c62dea5dcd16b530
10221285 <Valyajei.com@gmail.com>
2023-09-14 21:12:28 +0300

    Вторая задача

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,39 @@
-fun main() {
-    println("ВВедетие последовательность")
-    val r = readln()
-    var currentVal = r[0]
-    var count = 1
-    var itog = ""
-    for (item in r.substring(1)) {
-        if (currentVal == item) {
-            count++
-        } else {
-            if (count == 1) {
-                itog += currentVal
-            } else {
-                itog += "$currentVal$count"
-            }
-            count = 1
-            currentVal = item
+fun main()
+//{
+//    println("ВВедетие последовательность")
+//    val r = readln()
+//    var currentVal = r[0]
+//    var count = 1
+//    var itog = ""
+//    for (item in r.substring(1)) {
+//        if (currentVal == item) {
+//            count++
+//        } else {
+//            if (count == 1) {
+//                itog += currentVal
+//            } else {
+//                itog += "$currentVal$count"
+//            }
+//            count = 1
+//            currentVal = item
+//        }
+//    }
+//    if (count == 1) {
+//        itog += currentVal
+//    } else {
+//        itog += "$currentVal$count"
+//    }
+//    println(itog)
+//
+//}
+{
+    println("ВВедите:")
+    val a = readLine()
+    val b = a?.groupBy { it }?.eachCount()?.toSortedMap()
+    if (b != null){
+        for ((char, count) in b) {
+            println ("$char - $count")
         }
     }
-    if (count == 1) {
-        itog += currentVal
-    } else {
-        itog += "$currentVal$count"
-    }
-    println(itog)
 
 }
